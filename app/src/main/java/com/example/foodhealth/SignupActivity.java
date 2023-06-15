5edbf5566cf3547a47d895624c1cd2afc939dcad
I Kadek Ananda Krisna Wiralaksana <95567012+AnandaKrisna21@users.noreply.github.com>
2023-06-15 19:53:25 +0800

    Update SignupActivity.java

--- app/src/main/java/com/example/foodhealth/SignupActivity.java
+++ app/src/main/java/com/example/foodhealth/SignupActivity.java
@@ -21,16 +21,21 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
-    EditText edt_user, edt_date, edt_email,edt_pass;
-    RadioGroup radio_gender;
-    RadioButton radiobuttonselectgender;
+    EditText edt_user, edt_date, edt_phone, edt_email,edt_pass;
     Button btn_sign;
     TextView txt_login;
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://login-signup-7b7c9-default-rtdb.firebaseio.com");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +44,11 @@ public class SignupActivity extends AppCompatActivity {
 
         edt_user = findViewById(R.id.et_user);
         edt_date = findViewById(R.id.et_date);
+        edt_phone = findViewById(R.id.et_phone);
         edt_email = findViewById(R.id.et_mail);
         edt_pass = findViewById(R.id.et_pas);
         btn_sign = findViewById(R.id.bt_sign);
         txt_login = findViewById(R.id.txt_lgn);
-
-        //Radio Button for Gender
-        radio_gender = findViewById(R.id.rd_gender);
-        radio_gender.clearCheck();
-
 
         txt_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,65 +62,41 @@ public class SignupActivity extends AppCompatActivity {
         btn_sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int selectgender = radio_gender.getCheckedRadioButtonId();
-                radiobuttonselectgender = findViewById(selectgender);
-
                 //data
-                String fullname, date, email, password;
-                String textgender;
-                fullname = String.valueOf(edt_user.getText());
-                date = String.valueOf(edt_date.getText());
-                email = String.valueOf(edt_email.getText());
-                password = String.valueOf(edt_pass.getText());
+                final String fullname = edt_user.getText().toString();
+                final String date = edt_date.getText().toString();
+                final String phone = edt_phone.getText().toString();
+                final String email = edt_email.getText().toString();
+                final String password = edt_pass.getText().toString();
 
-                if (TextUtils.isEmpty(fullname)){
-                    Toast.makeText(SignupActivity.this, "Enter Fullname", Toast.LENGTH_SHORT).show();
-                    edt_user.setError("Fullname is Required");
-                    edt_user.requestFocus();
-                } else if (TextUtils.isEmpty(date)){
-                    Toast.makeText(SignupActivity.this, "Enter Date of Birth", Toast.LENGTH_SHORT).show();
-                    edt_date.setError("Date of Birth is Required");
-                    edt_date.requestFocus();
-                } else if (TextUtils.isEmpty(email)) {
-                    Toast.makeText(SignupActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
-                    edt_email.setError("Email is Required");
-                    edt_email.requestFocus();
-                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                    Toast.makeText(SignupActivity.this, "Please Re-enter Email", Toast.LENGTH_SHORT).show();
-                    edt_email.setError("Valid Email is Required");
-                    edt_email.requestFocus();
-                } else if (TextUtils.isEmpty(password)){
-                    Toast.makeText(SignupActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
-                    edt_pass.setError("Password is Required");
-                    edt_pass.requestFocus();
-                } else if (radio_gender.getCheckedRadioButtonId() == -1) {
-                    Toast.makeText(SignupActivity.this, "Please Select Your Gender", Toast.LENGTH_SHORT).show();
-                    radiobuttonselectgender.setError("Gender is Required");
-                    radiobuttonselectgender.requestFocus();
+                if (fullname.isEmpty() || date.isEmpty() || phone.isEmpty() || email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
                 } else {
-                    textgender = radiobuttonselectgender.getText().toString();
-                    registerUser(fullname,date,textgender,email,password);
+                    databaseReference.child("user").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.hasChild(phone)){
+                                Toast.makeText(SignupActivity.this, "Email is already", Toast.LENGTH_SHORT).show();
+                            } else {
+                                databaseReference.child("user").child(phone).child("fullname").setValue(fullname);
+                                databaseReference.child("user").child(phone).child("date").setValue(date);
+                                databaseReference.child("user").child(phone).child("email").setValue(email);
+                                databaseReference.child("user").child(phone).child("password").setValue(password);
+
+                               Toast.makeText(SignupActivity.this, "Register Succesfully", Toast.LENGTH_SHORT).show();
+                               startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                               finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
                 }
             }
         });
-    }
-
-    private void registerUser(String fullname, String date, String textgender, String email, String password) {
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(SignupActivity.this, "Sign Up Succesfull", Toast.LENGTH_SHORT).show();
-                            Intent a = new Intent(SignupActivity.this, LoginActivity.class);
-                            startActivity(a);
-                            finish();
-                        }
-                        else {
-                            Toast.makeText(SignupActivity.this, "Sign Up Failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
     }
 }
